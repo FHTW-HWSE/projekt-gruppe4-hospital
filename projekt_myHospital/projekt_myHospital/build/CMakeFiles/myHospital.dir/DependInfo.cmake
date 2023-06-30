f2cb6f2c67cd98955ca1c7a34fcb99f118be4739
Philipp Hoertnagl <108839362+philipphoertnagl@users.noreply.github.com>
2023-06-30 21:27:14 +0200

    adding compiled build folder

---
+++ projekt_myHospital/projekt_myHospital/build/CMakeFiles/myHospital.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/philipp/FH/HSWE_2/GIT/projekt-gruppe4-hospital/projekt_myHospital/projekt_myHospital/src/functions.c" "CMakeFiles/myHospital.dir/src/functions.c.o" "gcc" "CMakeFiles/myHospital.dir/src/functions.c.o.d"
+  "/Users/philipp/FH/HSWE_2/GIT/projekt-gruppe4-hospital/projekt_myHospital/projekt_myHospital/src/main.c" "CMakeFiles/myHospital.dir/src/main.c.o" "gcc" "CMakeFiles/myHospital.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ projekt_myHospital/projekt_myHospital/build/CMakeFiles/myHospital.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeCache.txt"
+  "CMakeFiles"
+  "Makefile"
+  "cmake_install.cmake"
+  "myHospital"
+  "CMakeFiles/myHospital.dir/src/functions.c.o"
+  "CMakeFiles/myHospital.dir/src/functions.c.o.d"
+  "CMakeFiles/myHospital.dir/src/main.c.o"
+  "CMakeFiles/myHospital.dir/src/main.c.o.d"
+  "myHospital"
+  "myHospital.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myHospital.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
