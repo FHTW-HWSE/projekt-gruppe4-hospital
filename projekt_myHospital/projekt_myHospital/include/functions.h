dcc2d168003c6e7fd8bb5f48b79265d399b21a3c
fav0u <90066018+fav0u@users.noreply.github.com>
2023-06-30 23:52:01 +0200

    Add files via upload

--- projekt_myHospital/projekt_myHospital/include/functions.h
+++ projekt_myHospital/projekt_myHospital/include/functions.h
@@ -12,12 +12,14 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
+//#include <conio.h>
 
 #define MAX_REG 100
 #define MAX_SEAT 25
 #define MAX_PAT 100
 #define MAX_ER 10
 #define MAX_NEIGH 100
+#define MAX_ARCHIVE 100
 
 typedef struct{
     char svNr[11];
@@ -60,17 +62,37 @@ typedef struct{
 
 }Neighbour;
 
+typedef struct {
+    char username[11];
+    char password[11];
+}Admin;
+
+typedef struct{
+    char svNr[11];
+    char vorname[20];
+    char nachname[20];
+    char gebdat[20];
+    char telefon[20];
+    char email[40];
+    char arrival_method[20];
+    time_t date_of_arrival;
+    char status[20];
+}Archive;
+
 void reset_seats(Seat seats[MAX_SEAT],Patient patients[MAX_PAT]);
 void reset_er(ER er[MAX_ER]);
-int load_register(Register reg[MAX_REG], Seat seats[MAX_SEAT], int record);
+int load_register(Register reg[MAX_REG], int record);
 void load_patient_data(Patient patients[MAX_PAT], int patient_record);
-void load_seat_data(Seat seats[MAX_SEAT]);
+void load_seat_data(Seat seats[MAX_SEAT],Patient patients[MAX_PAT]);
 void load_er_data(ER er[MAX_ER]);
 void assign_patient_to_seat(Seat seats[MAX_SEAT], Register reg[MAX_REG], Patient patients[MAX_PAT], int record);
 void send_patient_to_er(Seat seats[MAX_SEAT], Register reg[MAX_REG], Patient patients[MAX_PAT], ER er[MAX_ER]);
 void add_patient(Patient patients[MAX_PAT], int patient_record);
 void successful_treatment(Patient patients[MAX_PAT], ER er[MAX_ER]);
-void query_seat_neighbour(Neighbour neigh[MAX_NEIGH], Register reg[MAX_REG], Seat seats[MAX_SEAT]);
-void contact_trace(Neighbour neigh[MAX_NEIGH], Register reg[MAX_REG], Seat seats[MAX_SEAT]);
+void query_seat_neighbour(Neighbour neigh[MAX_NEIGH], Register reg[MAX_REG]);
+void contact_trace(Neighbour neigh[MAX_NEIGH]);
+void check_credentials(Admin admin, char username[11], char password[11]);
+void load_archive(Archive archives[MAX_ARCHIVE]);
+void hideInput(char* password, size_t maxLength);
 
 #endif //PROJEKT_FUNCTIONS_H
